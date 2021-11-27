6f19063e36a0b311c4c83f81221bac1c2012163f
Imran Hossain <umemous@gmail.com>
2021-11-27 14:11:12 +0600

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,13 @@
+function is_shop_manager() {
+    $user = wp_get_current_user();
+
+    if ( isset( $user->roles[0] ) && $user->roles[0] == 'shop_manager' ) {
+        return true;    // when user is shop manager
+    } else {
+        return false;   // when user is not shop manager
+    }
+}
+
+if ( is_shop_manager() ) {
+    // write code for shop_manager here
+}
